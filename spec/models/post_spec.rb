e0e2685ca878329f8412a712e6a66eb9b05502e9
Dylan Richards <dylanrichards@Dylans-MBP.home>
2015-07-16 17:56:05 -0400

    Test Post#next

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Post, :type => :model do
 
   it { should validate_presence_of  :title }
   it { should validate_presence_of  :text }
+
+  describe "#next" do
+    it 'finds the next post' do
+      post1 = Post.create(title: "post1", text: "post1")
+      post2 = Post.create(title: "post2", text: "post2")
+
+      expect(post1.next).to eq post2
+    end
+  end
 end
