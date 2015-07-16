193e5107a7b54e7a4ec87ba9b55bc9471033c2d3
Dylan Richards <dylanrichards@Dylans-MBP.home>
2015-07-16 14:00:45 -0400

    Implement next/prev

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   def pretty_time
     created_at.to_time.strftime('%B %e at %l:%M %p')
   end
+
+  def next
+    Post.where("id > ?", id).first
+  end
+
+  def prev
+    Post.where("id < ?", id).last
+  end
 end
