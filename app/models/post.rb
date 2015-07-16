958bbcf9a3eb6fdccc9690701f94d547c84405f9
Dylan Richards <dylanrichards@Dylans-MBP.home>
2015-07-16 16:32:19 -0400

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,16 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :text
+
   def pretty_time
     created_at.to_time.strftime('%B %e at %l:%M %p')
   end
 
   def next
-    Post.where("id > ?", id).first
+    where("id > ?", id).first
   end
 
   def prev
-    Post.where("id < ?", id).last
+    where("id < ?", id).last
   end
 end
